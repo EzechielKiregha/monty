1b7e2afc4f47f0663426b7489759336781b5c116
EzechielKiregha <ekiregha@gmail.com>
2024-01-19 10:03:01 +0200

    The opcode swap swaps the top two elements of the stack.

--- func_tool_swap.c
+++ func_tool_swap.c
@@ -8,26 +8,26 @@
  */
 void f_swap(stack_t **head, unsigned int counter)
 {
-        stack_t *h;
-        int len = 0, temp;
+	stack_t *h;
+	int len = 0, temp;
 
-        h = *head;
-        while (h)
-        {
-                h = h->next;
-                len++;
-        }
-        if (len < 2)
-        {
-                fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
-                fclose(bus.file);
-                free(bus.content);
-                free_stack(*head);
-                exit(EXIT_FAILURE);
-        }
-        h = *head;
-        temp = h->n;
-        h->n = h->next->n;
-        h->next->n = temp;
+	h = *head;
+	while (h)
+	{
+		h = h->next;
+		len++;
+	}
+	if (len < 2)
+	{
+		fprintf(stderr, "L%d: can't swap, stack too short\n", counter);
+		fclose(bus.file);
+		free(bus.content);
+		free_stack(*head);
+		exit(EXIT_FAILURE);
+	}
+	h = *head;
+	temp = h->n;
+	h->n = h->next->n;
+	h->next->n = temp;
 }
 
